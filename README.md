e306a4936e74ab792e4a836b292f2f86c03cdba1
Chris Scully <chris7scully@gmail.com>
2022-11-11 11:29:52 -0500

    added game_id argument passing to cmd

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Jeopardy Analytics
-This is an in-development repository that will scrape, parse, perform 
-analytics, and visualize data from Jeopardy.
+This is an in-development repository that will scrape, parse, perform analytics, 
+and visualize data from j-archive.com.
+
+## Scraping/Parsing J-Archive
+To scrape and parse episodes from j-archive.com, first determine the game IDs
+ you wish to analyze. This number can be found in the URL of the J-Archive 
+ contest (i.e. https://j-archive.com/showgame.php?game_id=4000). Run 
+ `python j_archive_scraper.py <first game_id> <last game_id>` to scrape and 
+ parse episodes from all contests between those IDs.
--- j_archive_scraper.py
+++ j_archive_scraper.py
@@ -1,5 +1,6 @@
 if __name__ == '__main__':
     import os
+    import sys
     from scraper.scraper_config import ScraperConfig
     from scraper.episode_scraper import scrape_episode
 
@@ -8,8 +9,11 @@ if __name__ == '__main__':
     ROBOTS_TXT_URL = 'http://www.j-archive.com/robots.txt'
     EPISODE_BASE_URL = 'http://www.j-archive.com/showgame.php?game_id='
 
+    start_ep_num = int(sys.argv[1])
+    end_ep_num = int(sys.argv[2])
+
     j_scraper = ScraperConfig(robots_txt_url=ROBOTS_TXT_URL)
-    for i in range(4000, 4001):
+    for i in range(start_ep_num, end_ep_num+1):
         print(f'Scraping/parsing episode #{i}')
         episode_csv = os.path.join(CSV_OUTPUT_DIR, 'episode', f'show_{i}.csv')
         episode_df = scrape_episode(j_scraper, i, HTML_PARSER, EPISODE_BASE_URL)
