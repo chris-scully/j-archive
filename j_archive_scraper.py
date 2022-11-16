4db8a1f6bf1ec0763d7dbebd33848b08c9981090
Chris Scully <chris7scully@gmail.com>
2022-11-16 16:44:33 -0500

    added error handling and logging to scraper

--- j_archive_scraper.py
+++ j_archive_scraper.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
-    import os
     import sys
+    from os import path
     from scraper.utils.scraper import Scraper
     from scraper.episode_scraper import scrape_episode
     from database.db_utils import df_to_db, game_table_schema
@@ -19,11 +19,18 @@ if __name__ == '__main__':
     j_scraper = Scraper(robots_txt_url=ROBOTS_TXT_URL)
     for episode_num in range(start_ep_num, end_ep_num+1):
         print(f'Scraping/parsing episode #{episode_num}')
-        episode_df = scrape_episode(j_scraper, episode_num, HTML_PARSER, EPISODE_BASE_URL)
-        df_to_db(df=episode_df, 
-                 project_id=project_id, 
-                 dataset='dev_question_data', 
-                 table_name='question_data', 
-                 table_schema=game_table_schema, 
-                 if_exists=if_exists
-                )
+        try:
+            episode_df = scrape_episode(j_scraper, episode_num, HTML_PARSER, EPISODE_BASE_URL)
+            df_to_db(df=episode_df, 
+                    project_id=project_id, 
+                    dataset='dev_question_data', 
+                    table_name='question_data', 
+                    table_schema=game_table_schema, 
+                    if_exists=if_exists
+                    )
+        except Exception as e:
+            path_to_log = path.join('log', 'scraper_error_game_id_log.txt')
+            log = open(path_to_log, 'a')
+            log.write(str(episode_num)+'\n')
+            log.close()
+            print('\tError Episode #'+str(episode_num)+':', e)
